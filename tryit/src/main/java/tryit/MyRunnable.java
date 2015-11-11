66ac83641b8c94f30a41c6a835951a3faa5e2e05
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2015-11-11 23:29:54 +0000

    tryit

--- tryit/src/main/java/tryit/MyRunnable.java
+++ tryit/src/main/java/tryit/MyRunnable.java
@@ -2,7 +2,7 @@ package tryit;
 
 import org.apache.commons.javaflow.Continuation;
 
-class MyRunnable implements Runnable {
+public class MyRunnable implements Runnable {
 	public void run() {
 		System.out.println("started!");
 		for (int i = 0; i < 10; i++)
