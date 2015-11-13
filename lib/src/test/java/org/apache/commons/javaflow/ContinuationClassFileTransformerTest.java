2602993d83650e2f63493996d9ceb0fa71c79546
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2015-11-13 11:41:40 +0000

    conf of classes to be instrumented

--- lib/src/test/java/org/apache/commons/javaflow/ContinuationClassFileTransformerTest.java
+++ lib/src/test/java/org/apache/commons/javaflow/ContinuationClassFileTransformerTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.lang.instrument.IllegalClassFormatException;
 import java.util.Collections;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.*;
 
 /**
@@ -15,20 +17,25 @@ import static org.mockito.Mockito.*;
 public class ContinuationClassFileTransformerTest {
 
 	byte[] testClz = new byte[]{5, 6, 7};
+	byte[] testClzTransformed = new byte[]{15, 16, 17};
 
 	@Test
 	public void transformsPrefixedPositive() throws IllegalClassFormatException {
 		ResourceTransformer transformer = mock(ResourceTransformer.class);
+		when(transformer.transform(testClz)).thenReturn(testClzTransformed);
 		ContinuationClassFileTransformer t = ContinuationClassFileTransformer.withResourceTransformer(Collections.singletonList("tryit."), transformer);
-		t.transform(null, "tryit.Main", null, null, testClz);
+		byte[] clz = t.transform(null, "tryit.Main", null, null, testClz);
+		assertArrayEquals(testClzTransformed, clz);
 		verify(transformer).transform(testClz);
 	}
 
 	@Test
 	public void transformsPrefixedNegative() throws IllegalClassFormatException {
 		ResourceTransformer transformer = mock(ResourceTransformer.class);
+		when(transformer.transform(testClz)).thenReturn(testClzTransformed);
 		ContinuationClassFileTransformer t = ContinuationClassFileTransformer.withResourceTransformer(Collections.singletonList("tryit."), transformer);
-		t.transform(null, "x.Main", null, null, testClz);
+		byte[] clz = t.transform(null, "x.Main", null, null, testClz);
+		assertNull(clz);
 		verify(transformer, never()).transform(testClz);
 	}
 
